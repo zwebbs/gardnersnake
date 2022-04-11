69467f904c4f1e68fb0bab300a0c65c11cf00d10
zwebbs <zwebbs2021@gmail.com>
2022-04-11 16:33:37 -0500

    began work on configuration helper

--- src/configurationhelper.py
+++ src/configurationhelper.py
@@ -4,7 +4,16 @@
 # Purpose: a class definition for a configurationHelper that makes
 #  snakemake worflows easier to construct
 
+# module imports
+from .pathutils import get_verified_path
 
+
+# class definitions
+#-----------------------
+
+# class ConfigurationHelper: object which
+# cleans up much of the code linking run
+# conifgurations to snakefile rules.
 class ConfigurationHelper:
     def __init__ (self, cfg_dict):
         self.d = cfg_dict
@@ -16,8 +25,11 @@ class ConfigurationHelper:
     def get_gobal_param(self, param):
         pass
 
-    def get_rule_param(self, rule, param, ispath=False, exists=False):
-        pass
+    def get_rule_param(self, rule, param, pathtype=None,
+                       ispath=False, exists=False):
+        if ispath: return get_verified_path(path, pathype, exists)
+        else:
+            pass
 
     def get_rule_resources(self, rule, logdir="logs/", job_id):
         pass
