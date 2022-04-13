b0806280240a20b790c3293e14b243f7ddffab99
zwebbs <zwebbs2021@gmail.com>
2022-04-13 13:15:53 -0500

    continued work on ConfigurationHelper. still need to review resource accesss commands and test all

--- src/confighelper.py
+++ src/confighelper.py
@@ -5,72 +5,102 @@
 #  snakemake worflows easier to construct
 
 # module imports
+from collections import OrderedDict
 from .pathutils import get_verified_path
-from .exceptions import RuleConfigurationError
-from .exceptions import GlobConfigurationError
+from .exceptions import eprint
+from .exceptions import UserError
+from .exceptions import ConfigParameterError
+from .exceptions import ConfigRuleParameterError
 
 # class definitions
-#-----------------------
+#------------------------------------------------------------------------------
 
 # class ConfigurationHelper: object which
 # cleans up much of the code linking run
 # conifgurations to snakefile rules.
 class ConfigurationHelper:
     def __init__ (self, cfg_dict):
-        self.cfg = self.globs = cfg_dict.copy()
+        self.cfg = cfg_dict
+        self._validate_config_dict(self.cfg)
+        self.globs = self.cfg.copy()
         self.rule_params = self.cfg.pop("rule_params")
         self.resource_list = [
             "walltime","nodes",
             "processors_per_node"
         ]
 
-    # internal methods
-    #-------------------
+    # define _validate_config_dict() function to ensure minimum requirements
+    # for the passed configuration dict --including type checking the dict
+    def _validate_config_dict(self,cfg):
+        validation_codes = []
+        keys_to_check = ["analysis_id", "workdir", "rule_params"]
+        outcome = lambda x: validation_codes.append(x)
 
-    # _get_global_param()
-    # returns global parameters with optional path handling
+        # test the type of the passed dict
+        outcome(type(cfg) in [dict, OrderedDict])
+
+        # test default required keys
+        try:
+            for k in keys_to_check:
+                outcome(k in cfg.keys())
+        except KeyError as keyexc:
+            msg1 = "Could not Validate Configuration in ConfiguationHelper."
+            mgs2 = "Please check the listed keys."
+            msg = "\n".join(msg1,msg2)
+            raise ConfigParameterError(cfg, msg, keys_to_check) from keyexc
+
+        # return 0 if the config is validated
+        if all(validation_codes):
+            return
+        else: # raise user error upon unsuccessful validation
+            eprint(f"passed config: {cfg}")
+            raise UserError("Could not validate configuration dictionary")
+
+    # define _get_global_param() internal method to return a top level
+    # parameter from the passed configuration with optional path handling
     def _get_global_param(self, param, ispath=False,
                          pathtype=None, exists=False):
         # access the parameter, check if we need to do path handling
         # and return accordingly
         try: gparam = self.globs[param]
-        except KeyError as kexc:
-            msg = "could not find entry in configuration"
-            raise GlobConfigurationError(msg,param) from kexc
+        except KeyError as ke:
+            msg = "Key Error"
+            perr = ConfigParameterError(self.globs,msg,param)
+            raise perr from ke
         if not ispath: return gparam
         else:
-            return get_verified_path(gparam,pathtype, exists)
+            return get_verified_path(gparam, pathtype, exists)
 
-    # _get_rule_param()
-    # returns rule parameters with optional path handling
+    # define _get_rule_param() internal method to return a rule-level
+    # parameter from the passed configuration with optional path handling
     def _get_rule_param(self, rule, param, ispath=False,
                         pathtype=None, exists=False):
         # access the parameter, check if we need to do path handling
-        # and return accordingly. if its not in the config, raise
-        # a custom exception (RuleConfigurationError)
-        try:rparam = self.rule_params[rule][param]
-        except KeyError as kexc:
-            msg = "could not find entry in configuration"
-            raise RuleConfigurationError(msg,rule,param) from kexc
+        # and return accordingly.
+        try: rparam = self.rule_params[rule][param]
+        except KeyError as ke:
+            msg = "Key Error"
+            rperr = ConfigRuleParameterError(self.rule_params,msg,param,rule)
+            raise rperr from ke
         if not ispath: return rparam
         else:
-            return get_verified_path(rparam, pathype, exists)
+            return get_verified_path(rparam, pathype, exists
 
-
-    # exposed methods
-    #-------------------
-
-    # get_global_param()
-    # wrapper for internal method _get_global_param
+    # define get_global_param(): exposed wrapper for internal
+    # method _get_global_param()
     def get_global_param(self, param, ispath=False,
                          pathtype=None, exists=False):
         return self._get_global_param(param, ispath, pathtype, exists)
 
-    # get_rule_param()
-    # wrapper for internal method _get_rule_param
+    # define get_rule_param(): exposed wrapper for internal
+    # method _get_rule_param()
     def get_rule_param(self, rule, param, ispath=False,
                        pathtype=None, exists=False):
         return self._get_rule_param(rule, param, ispath, pathtype, exists)
+
+
+
+
 
     # get_rule_resources()
     # returns the standard resource set as a dictionary according
