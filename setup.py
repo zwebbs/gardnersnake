c4f6e472d2423bd1de5e2fb0ad30ca8d7765171e
zwebbs <zwebbs2021@gmail.com>
2022-04-19 22:17:00 -0500

    basic packaging and pushed unstable to pypi

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gardnersnake",
+    version="0.0.2",
+    author="Zach Weber",
+    author_email="zach.weber.813@gmail.com",
+    description="Utilities for writing concise snakemake workflows",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zwebbs/gardnersnake",
+    project_urls={
+        "Bug Tracker": "https://github.com/zwebbs/gardnersnake/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
