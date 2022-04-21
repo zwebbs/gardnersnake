028b5cf7e37f1977f09dab48eb022ec8cfda5c95
zwebbs <zwebbs2021@gmail.com>
2022-04-21 16:39:33 -0500

    working solution for check_directory hopefully

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gardnersnake",
-    version="0.1.4",
+    version="0.1.5",
     author="Zach Weber",
     author_email="zach.weber.813@gmail.com",
     description="Utilities for writing concise snakemake workflows",
@@ -24,6 +24,6 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
     entry_points={'console_scripts': {
-        'check_directory=sripts.check_directory:main'}
+        'check_directory=scripts.check_directory:main'}
     }
 )
