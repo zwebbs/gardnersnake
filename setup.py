cb5766d71a20c6a01c66d50851d3454d2126fa4f
zwebbs <zwebbs2021@gmail.com>
2022-07-20 23:58:47 -0500

    fixed bug in setup.py (missing comma)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gardnersnake",
-    version="0.4.1"
+    version="0.4.1",
     author="Zach Weber",
     author_email="zach.weber.813@gmail.com",
     description="Utilities for writing concise snakemake workflows",
